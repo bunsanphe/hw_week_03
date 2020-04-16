a89c96aafd674c0bfe0e7e51b5c9c0868f13a4ea
Heng <bunsanphe@gmail.com>
2020-04-15 23:03:11 -0700

    Adding HW

---
+++ Develop/script.js
@@ -0,0 +1,67 @@
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+
+document.getElementById("generate").onclick = function () {
+  start()
+}
+
+function start() {
+
+var pwLength = 0;
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    specialChar = ["@", "$", "!", "-"],
+    upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function pwLengthValidate(pwLength) {
+    return (pwLength >= 8 && pwLength <= 128);
+    }
+
+    while (! pwLengthValidate(pwLength)) {
+    pwLength = parseInt( prompt("Provide a password lenght between 8 an 128") );
+    }
+    console.log(pwLength);
+
+    var options = {
+        "lowerCaseOp": confirm("Would you like lowercase letter in your password?"),
+        "upperCaseOp": confirm("Would you like uppercase letters in your password?"),
+        "numericOp": confirm("Would you like numbers in your password?"),
+        "specialCharOp": confirm("Would you like special characte letters in your password?")
+    };
+
+function genPW(options) {
+    var password = "";
+    var possibleChar = [];
+
+    if(options.lowerCaseOp) {
+        possibleChar = possibleChar.concat(lowerCase);
+    }
+
+    if(options.upperCaseOp) {
+        possibleChar = possibleChar.concat(upperCase);
+    }
+
+    if(options.numericOp) {
+        possibleChar = possibleChar.concat(numeric);
+    }
+
+    if(options.specialCharOp) {
+        possibleChar = possibleChar.concat(specialChar);
+    }
+    console.log(possibleChar);
+
+    for(var i = 0; i <= pwLength-1; i++) {
+        password = password + possibleChar[Math.floor(Math.random() * possibleChar.length)];
+    }
+    return password;
+}
+console.log(genPW(options));
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", isPWLenghtCorrect);
+
+// text box dsiplay area
+document.getElementById("password").placeholder = genPW(options);
+}
